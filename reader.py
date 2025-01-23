f8061faea36c7175edad81b06831f1cf7b30b2a3
Amer Khalil <amerkhalil@Mac.hsd1.il.comcast.net>
2025-01-22 18:59:44 -0600

    First stage of functionality

--- reader.py
+++ reader.py
@@ -1,8 +1,9 @@
 import constants
 import pandas as pd
-from utils.excel_utils import mergeBooks
-#invoice_path = f'{constants.DATA_PATH}/{constants.CUSTOMER_DATA_FILE_NAME}'
-#invoice_df = pd.read_excel(invoice_path, sheet_name=constants.INVOICE_SHEET_NAME)
-#invoice_df.drop(columns=[col for col in invoice_df.columns if 'Unnamed' in col], inplace=True)
+from utils.excel_utils import mergeBooks, read_sheet, merge_sheets
 
-mergeBooks()
+inv = read_sheet(constants.INVOICE_DATA_FILE_NAME, constants.INVOICE_SHEET_NAME, ["Name", "Item", "Qty"])
+#cust = read_sheet(constants.AGGREGATED_BOOK, constants.AGGREGATED_BOOK_CUSTOMER_SHEET_NAME)
+#prod = read_sheet(constants.AGGREGATED_BOOK, constants.AGGREGATED_BOOK_PRODUCT_SHEET_NAME)
+
+print(inv.head())
--- utils/excel_utils.py
+++ utils/excel_utils.py
@@ -1,15 +1,39 @@
 import pandas as pd
+import os
+import constants
 
-def mergeBooks(book1_name: str, book2_name: str, book3_name: str, sheet1_name: str, sheet2_name: str, sheet3_name: str) -> None:
+def mergeBooks(invoice_book_name: str, customer_book_name: str, product_book_name: str,
+                invoice_sheet_name: str, customer_sheet_name: str, product_sheet_name: str) -> None:
 
+    '''
+    Make One workbook that has all of the sheets from the parent books.
+    Gives consistent format to work off of.
+    '''
     # Load specific sheets from the source Excel workbooks
-    sheet1 = pd.read_excel(book1_name, sheet_name=sheet1_name)
-    sheet2 = pd.read_excel(book2_name, sheet_name=sheet2_name)
-    sheet3 = pd.read_excel(book3_name, sheet_name=sheet3_name)
+    sheet1 = pd.read_excel(invoice_book_name, sheet_name=invoice_sheet_name)
+    sheet2 = pd.read_excel(customer_book_name, sheet_name=customer_sheet_name)
+    sheet3 = pd.read_excel(product_book_name, sheet_name=product_sheet_name)
 
     # Create a new Excel writer to save the combined sheets
-    with pd.ExcelWriter("aggregated_book.xlsx", engine="openpyxl") as writer:
+    with pd.ExcelWriter(f"{constants.DATA_PATH}/aggregated_book.xlsx", engine="openpyxl") as writer:
         # Write each sheet to the new workbook
-        sheet1.to_excel(writer, sheet_name="Sheet1", index=False)
-        sheet2.to_excel(writer, sheet_name="Sheet2", index=False)
-        sheet3.to_excel(writer, sheet_name="Sheet3", index=False)
+        sheet1.to_excel(writer, sheet_name=constants.AGGREGATED_BOOK_INVOICE_SHEET_NAME, index=False)
+        sheet2.to_excel(writer, sheet_name=constants.AGGREGATED_BOOK_CUSTOMER_SHEET_NAME, index=False)
+        sheet3.to_excel(writer, sheet_name=constants.AGGREGATED_BOOK_PRODUCT_SHEET_NAME, index=False)
+
+
+def clear_unnamed_columns(df: pd.DataFrame):
+    df.drop(columns=[col for col in df.columns if 'Unnamed' in col], inplace=True)
+
+def read_sheet(book_name: str, sheet_name: str, cols: list) -> pd.DataFrame:
+    df = pd.read_excel(book_name, sheet_name=sheet_name, usecols=cols)
+    clear_unnamed_columns(df)
+
+    return df
+
+def merge_sheets():
+    '''
+    Take the sheets in the aggregate book and make the report from it
+    '''
+
+    pass
