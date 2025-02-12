da4846f745024a9605d3dabc4ed1f758d528e9b4
Amer Khalil <amerkhali31@gmail.com>
2025-02-11 23:28:17 -0600

    V2 Updates

--- reader.py
+++ reader.py
@@ -7,7 +7,7 @@ import re
 # Choose which columns to read out of the quickbooks generated excel report
 invoice_columns_to_use = "Date Num Memo Name Qty Amount Item".split() + ["Sales Price"]
 prod_cols_to_use = "Item Description U/M Price UPC FED_DESCRIPTION BRAND UNIT".split() + ["UNIT DESCRIPTION"]
-cust_cols_to_use = "Customer FEIN LICENSE".split() + ["Bill to 1", "Bill to 2", "Sales Tax Code", "Customer Type"]
+cust_cols_to_use = ["Customer", "FEIN", "LICENSE", "Bill to 1", "Bill to 2", "Sales Tax Code", "Customer Type"]
 
 
 # Read The Excel Report into Pandas DataFrames
@@ -15,6 +15,7 @@ inv = read_sheet(constants.INVOICE_DATA_FILE_NAME, constants.INVOICE_SHEET_NAME,
 cust = read_sheet(constants.CUSTOMER_DATA_FILE_NAME, constants.CUSTOMER_SHEET_NAME, cust_cols_to_use)
 prod = read_sheet(constants.PRODUCT_DATA_FILE_NAME, constants.PRODUCT_SHEET_NAME, prod_cols_to_use)
 
+print(f'Inv Initial: {inv.shape}')
 
 # Prepare DF's for merge
 inv['Item'] = inv['Item'].str.replace(r'\(.*\)', '', regex=True).str.strip()
@@ -37,16 +38,16 @@ print(f'Merge2: {merge2.shape}')
 merge2['FED_DESCRIPTION'] = merge2['FED_DESCRIPTION'].fillna('')
 merge2['BRAND'] = merge2['BRAND'].fillna('')
 merge2['UNIT'] =  merge2['UNIT'].fillna(0)
+merge2['FED_DESCRIPTION'] = merge2['FED_DESCRIPTION'].replace({'VAPOR': 'VAPOR PRODUCT'}).str.strip()
 
 condition2 = merge2['BRAND'].apply(
     lambda x: any(brand.lower() in x.lower() for brand in constants.MS_BRANDS) if isinstance(x, str) else False
 )
 conditions = [
-    merge2['FED_DESCRIPTION'].isin(['E-liquid Product', 'Vapor Products']),
+    merge2['FED_DESCRIPTION'].isin(['E-liquid Product', 'VAPOR PRODUCT']),
     condition2
 ]
-choices = ['ECIG', 'MS']
-
+choices = ['IL-ECIG', 'IL-MS']
 
 # Build th New DataFrame
 df = pd.DataFrame(columns=constants.TP_1_IL_STRUCTURE)
@@ -61,22 +62,22 @@ df["Schedule Code"] = merge2['Customer Type'].map(constants.SCHEDULE_CODES)
 df["Document Date"] = merge2['Date'].dt.strftime('%m-%d-%Y')
 df["Document Type"] = constants.DOCUMENT_TYPE
 df["Document Number"] = merge2['Num']
-df["Type of Customer"] = merge2['Customer Type']
+df["Type of Customer"] = merge2['Customer Type'].replace({'Retail': 'Retailer'})
 df["Name"] = merge2['Customer']
 df["Street Address"] = merge2['Bill to 1']
 df["City"] = merge2['Bill to 2'].apply(lambda x: x.split(',')[0] if isinstance(x, str) and ',' in x else None)
 df["State"] = merge2['Bill to 2'].apply(lambda x: x.split(',')[1].strip().split(' ')[0] if isinstance(x, str) and ',' in x else None)
 df["Country"] = constants.COUNTRY
 df["Zip"] = merge2['Bill to 2'].apply(lambda x: x.split(',')[1].strip().split(' ')[1] if isinstance(x, str) and ',' in x and len(x.split(',')) > 1 and len(x.split(',')[1].strip().split(' ')) > 1 else None)
-df["Customer FEIN"] = merge2['FEIN']
-df["Customer ID"] = merge2['LICENSE']
+df["Customer FEIN"] = merge2['FEIN'].apply(lambda x: re.sub(r'[^0-9]', '', str(x)) if pd.notna(x) and str(x).strip() else '999999999')
+df["Customer ID"] = merge2['LICENSE'].apply(lambda x: re.sub(r'[^a-zA-Z0-9]', '', str(x)) if pd.notna(x) and str(x).strip() else 'CT99999')
 df["Fed Description"] = merge2['FED_DESCRIPTION']
-df["State Description"] = np.select(conditions, choices, default='OTP')
+df["State Description"] = np.select(conditions, choices, default='IL-OTP')
 df["MSA Status"] = constants.MSA_STATUS
-df["Price"] = "N/A"
+df["Price"] = ""
 df["Tax Jurisdiction"] = merge2['Sales Tax Code']
 df["UPC Number"] = merge2['UPC']
-df["UPCs Unit of Measure"] = merge2['U/M']
+df["UPCs Unit of Measure"] = merge2['U/M'].apply(lambda x: re.search(r'\((.*?)\)', str(x)).group(1) if pd.notna(x) and re.search(r'\((.*?)\)', str(x)) else 'BOX')
 df["Product Description"] = merge2['Description']
 df["Brand Family"] = merge2['BRAND']
 df["Unit"] = merge2['UNIT']
@@ -87,13 +88,30 @@ df["Quantity"] = merge2['Qty'] * merge2['UNIT']
 
 # In Progress
 df['Manufacturer'] = merge2['BRAND'].apply(lambda x: constants.MANUFACTURERS.get(x, 'N/A'))
-df["Manufacturer EIN"] = "N/A"  # TODO - create map for manufacturer EINs to manufacturers
+df["Manufacturer EIN"] = "999999999"  # TODO - create map for manufacturer EINs to manufacturers
 
 # Post Process
 filtered_df = df[(df['Unit'] != 0)].sort_values(by='Document Number')
 filtered_df['Unit'] = 1
 filtered_df.drop(['Temp_Price', 'Temp_Unit'], axis=1, inplace=True)
 
+print(f'Final: {filtered_df.shape}')
 
 # Convert Data to Excel Sheet
 filtered_df.to_excel('TP_1_IL_Report_V1.xlsx', index=False)
+
+# Find missing invoices after merge1 (missing Items)
+missing_after_merge1 = inv[~inv['Item'].isin(prod['Item'])]
+missing_after_merge1.to_excel("missing_after_merge1.xlsx", index=False)
+
+# Find missing invoices after merge2 (missing Customers)
+missing_after_merge2 = merge1[~merge1['Customer'].isin(cust['Customer'])]
+missing_after_merge2.to_excel("missing_after_merge2.xlsx", index=False)
+
+# Find missing invoices after filtering (Unit == 0)
+missing_after_filtering = merge2[merge2["UNIT"] == 0]
+missing_after_filtering.to_excel("missing_after_filtering.xlsx", index=False)
+
+print(f"Missing after first merge: {missing_after_merge1.shape[0]}")
+print(f"Missing after second merge: {missing_after_merge2.shape[0]}")
+print(f"Missing after filtering: {missing_after_filtering.shape[0]}")
